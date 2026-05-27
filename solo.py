9432c19ef7b79f429e3a11441ab845cd546b0760
laenor-tenorio5546 <laenor.henrique@estudante.ifgoiano.edu.br>
2026-05-27 11:40:02 -0300

    Update solo.py

--- solo.py
+++ solo.py
@@ -821,8 +821,7 @@ st.markdown("""
 with st.sidebar:
     st.image(
         "https://cdn-icons-png.flaticon.com/512/2909/2909763.png",
-        width=70//
-    )
+        width=70)
     st.markdown("### 🌱 Sistema Inteligente")
     st.markdown("""
     • Avaliação da fertilidade  
